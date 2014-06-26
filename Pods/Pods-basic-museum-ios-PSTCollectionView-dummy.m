182b3f4863852a046a2c351e7c28cd02043441d3
Adam Gleichsner <amg188@case.edu>
2014-06-26 16:47:58 -0400

    added CocoaPods

---
+++ Pods/Pods-basic-museum-ios-PSTCollectionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_basic_museum_ios_PSTCollectionView : NSObject
+@end
+@implementation PodsDummy_Pods_basic_museum_ios_PSTCollectionView
+@end
---
+++ Pods/Pods-basic-museum-iosTests-SDWebImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_basic_museum_iosTests_SDWebImage : NSObject
+@end
+@implementation PodsDummy_Pods_basic_museum_iosTests_SDWebImage
+@end
